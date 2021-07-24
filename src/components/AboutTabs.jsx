c06b5133b8b8330e4694446cdaa986cb443feae4
Journey Cruz <journey.cruz@gmail.com>
2021-07-24 11:48:54 -0500

    add company logos and make sure they link to software website/documentation

--- src/components/AboutTabs.jsx
+++ src/components/AboutTabs.jsx
@@ -22,47 +22,264 @@ class AboutTabs extends Component {
                   </TabList>
                   <div className="space"></div>
                   <TabPanel>
-                    <div className="single-tab-content">
-                      <ul>
-                        <li className="text-center">
-                          I have experience working with the following software:
-                          {/* Experience working with the following software: HTML,
+                    <div className="tabContainer">
+                      <div className="single-tab-content">
+                        <ul>
+                          <li className="text-center">
+                            Experience working with the following software:
+                            {/* Experience working with the following software: HTML,
                           CSS, JavaScript, jQuery, Bootstrap, APIs, JSON, AJAX,
                           Firebase, React.js, Heroku, Git, GitHub, AWS, MySQL,
                           MongoDB, Mongoose, Sequelize, Handlebars, Node.js,
                           Express.js, MVCs, sessions, test writing, algorithms,
                           and data structures. */}
-                        </li>
-                        {/* <li>
+                          </li>
+                          {/* <li>
                           My technical experience, work ethic, and diligence
                           make me an advantageous candidate for any development team.
                         </li> */}
-                      </ul>
-                      <div className="row">
-                        <ul className="col-md-6">
-                          <li>HTML</li>
-                          <li>CSS</li>
-                          <li>Git</li>
-                          <li>JavaScript</li>
-                          <li>jQuery</li>
-                          <li>Bootstrap</li>
-                          <li>SOAP and REST API's</li>
-                          <li>JSON</li>
-                          <li>AJAX</li>
-                          <li>MySQL</li>
                         </ul>
-                        <ul className="col-md-6">
-                          <li>MongoDB</li>
-                          <li>React</li>
-                          <li>Node</li>
-                          <li>Heroku</li>
-                          <li>Amazon Web Services</li>
-                          <li>Mongoose</li>
-                          <li>Handlebars</li>
-                          <li>Sequelize</li>
-                          <li>Express</li>
-                          <li>Jest</li>
-                        </ul>
+                        <div className="row">
+                          <ul className="col-md-6">
+                            <li>
+                              <a
+                                href="https://www.javascript.com/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLI"
+                                  src="/assets/images/html5-logo-31816.png"
+                                  alt="HTML, CSS, Javascript"
+                                />
+                              </a>
+                            </li>
+                            <li>
+                              {" "}
+                              <a
+                                href="https://jquery.com/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLIsmall"
+                                  src="/assets/images/jquery-1-logo-png-transparent.png"
+                                  alt="jQuery"
+                                />
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://git-scm.com/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLI"
+                                  src="/assets/images/gitlogoorange.png"
+                                  alt="Git"
+                                />
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://getbootstrap.com/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLIlarge"
+                                  src="/assets/images/bootstraplogolong.png"
+                                  alt="Bootstrap"
+                                />
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://expressjs.com/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLI"
+                                  src="/assets/images/express-logo.png"
+                                  alt="Express"
+                                />
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://rapidapi.com/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLIlarge"
+                                  src="/assets/images/rapidAPIlogo.png"
+                                  alt="RapidAPI"
+                                />
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://www.postman.com/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLI"
+                                  src="/assets/images/postman-logo.png"
+                                  alt="Postman"
+                                />
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://json5.org/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLIsmall"
+                                  src="/assets/images/json-5-logo-png-transparent.png"
+                                  alt="JSON"
+                                />
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://www.mysql.com/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLIlarge"
+                                  src="/assets/images/mysql-logo.png"
+                                  alt="MySQL"
+                                />
+                              </a>
+                            </li>
+                          </ul>
+                          <ul className="col-md-6">
+                            <li>
+                              <a
+                                href="https://www.mongodb.com/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLI"
+                                  src="/assets/images/mongodblogo.png"
+                                  alt="MongoDB"
+                                />
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://reactjs.org/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLIlarge"
+                                  src="/assets/react-logo.png"
+                                  alt="React"
+                                />
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://nodejs.org/en/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLI"
+                                  src="/assets/images/nodejs-logo.png"
+                                  alt="Node"
+                                />
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://www.heroku.com/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLIlarge"
+                                  src="/assets/images/heroku-logotype-horizontal-purple.png"
+                                  alt="Heroku"
+                                />
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://aws.amazon.com/free/?trk=ps_a131L0000085EJuQAM&trkCampaign=acq_paid_search_brand&sc_channel=ps&sc_campaign=acquisition_US&sc_publisher=google&sc_category=core-main&sc_country=US&sc_geo=NAMER&sc_outcome=acq&sc_detail=amazon%20web%20services&sc_content=Brand_amazon_web_services_e&sc_segment=423740514695&sc_medium=ACQ-P|PS-GO|Brand|Desktop|SU|Core-Main|Core|US|EN|Text&s_kwcid=AL!4422!3!423740514695!e!!g!!amazon%20web%20services&ef_id=EAIaIQobChMIqfii1pP88QIVdm1vBB1BJwqTEAAYASAAEgLAO_D_BwE:G:s&s_kwcid=AL!4422!3!423740514695!e!!g!!amazon%20web%20services&all-free-tier.sort-by=item.additionalFields.SortRank&all-free-tier.sort-order=asc&awsf.Free%20Tier%20Types=*all&awsf.Free%20Tier%20Categories=*all"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLIlarge"
+                                  src="/assets/images/aws-logo.png"
+                                  alt="Amazon Web Services"
+                                />
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://mongoosejs.com/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLI"
+                                  src="/assets/images/mongoose-logo.png"
+                                  alt="Mongoose"
+                                />
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://handlebarsjs.com/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLIlarge"
+                                  src="/assets/images/handlebars-js.png"
+                                  alt="Handlebars"
+                                />
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://sequelize.org/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLI"
+                                  src="/assets/images/sequelize-logo.png"
+                                  alt="Sequelize"
+                                />
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://jestjs.io/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                              >
+                                <img
+                                  className="softwareLIlarge"
+                                  src="/assets/images/jest-logo-png-transparent.png"
+                                  alt="Jest"
+                                />
+                              </a>
+                            </li>
+                          </ul>
+                        </div>
                       </div>
                     </div>
                   </TabPanel>
